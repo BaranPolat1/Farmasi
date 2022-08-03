da552df538340611c8998971d5705127d64650b0
BaranPolat1 <57008355+BaranPolat1@users.noreply.github.com>
2022-08-03 03:02:52 +0300

    Delete Farmasi_Project_Web directory

--- Farmasi_Project_Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Farmasi_Project_Web.Models
-{
-  public class ErrorViewModel
-  {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-  }
-}
